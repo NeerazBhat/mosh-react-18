f274238b3fad42258b633296ba8e53e9e823b34e
Niraj Bhat <niraj@fusemachines.com>
2023-07-07 19:12:11 +0545

    feat: removed unnecessary code

--- building-components/src/App.tsx
+++ building-components/src/App.tsx
@@ -1,44 +1,14 @@
 import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
 
 function App() {
-  const [showAlert, setShowAlert] = useState(false);
   const [cartItems, setCartItems] = useState(["Mouse", "Laptop"]);
-
-  const listData = ["Nepal", "India", "China"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
 
   return (
     <main className="p-3">
       <Navbar cartItemsCount={cartItems.length} />
       <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
-      {showAlert && (
-        <Alert onClose={() => setShowAlert(false)}>
-          Hello <span className="fw-bold">World</span>
-        </Alert>
-      )}
-      <ListGroup
-        title="Country List"
-        items={listData}
-        onSelectItem={handleSelectItem}
-      />
-      <Button
-        variant="warning"
-        className="mt-3"
-        onClick={() => setShowAlert(true)}
-      >
-        Toggle Alert
-      </Button>
-      <div className="d-flex justify-content-between mt-3">
-        <Button variant="secondary">Prev</Button>
-        <Button onClick={() => console.log("Next page rendred")}>Next</Button>
-      </div>
     </main>
   );
 }
